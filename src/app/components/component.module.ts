f333b5bde9d901d34b8d372c8c11274f26054865
Albert Panesse <albert.panesse@gmail.com>
2020-10-03 20:46:16 +0700

    ok good

---
+++ src/app/components/component.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+
+import { ImageSliderComponent } from "./image-slider/image-slider.component";
+
+@NgModule({
+  declarations: [ImageSliderComponent],
+  imports: [],
+  providers: [],
+  exports: [ImageSliderComponent]
+})
+export class ComponentModule {}
--- src/app/layouts/default-layout.module.ts
+++ src/app/layouts/default-layout.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { ComponentModule } from "../components/component.module";
+
 import { DefaultLayoutComponent } from "./default-layout/default-layout.component";
 import { HomePageComponent } from "../pages/home-page/home-page.component";
 
@@ -19,7 +21,7 @@ export const defaultRoutes: Routes = [
 
 @NgModule({
   declarations: [DefaultLayoutComponent, HomePageComponent],
-  imports: [RouterModule.forChild(defaultRoutes)],
+  imports: [ComponentModule, RouterModule.forChild(defaultRoutes)],
   providers: []
 })
 export class DefaultLayoutModule {}
